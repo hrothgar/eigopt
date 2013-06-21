8ecf181f74cf8931d76797ff8692c9832740a54e
hrothgar <hrothgarrrr@gmail.com>
2013-06-21 19:41:07 +0300

    Depth-first search implemented, I think

---
+++ eigopt_multi_mesh_depth.m
@@ -0,0 +1,176 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [hist, boxes] = eigopt_multi_mesh(funname, b0, b1, pars, opt)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% VARIABLES
+%    boxes(j).status
+%          1 = iterating
+%          0 = inactive; bounds proved it irrelevant
+%         -1 = inactive; split into smaller cells
+%
+% BUGS
+%   only supports minimization right now, not maximation
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% INITIALIZATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% convert minmax to a sign constant
+opt.minmax = 0; % for now, let's just keep this simple
+opt.minmax = 1 - 2*boolean(opt.minmax);
+
+% the dimensions of the problem must be consistent
+dim = length(b0);
+dim2 = length(b1);
+if (dim ~= dim2)
+    error('lengths of b0 and b1 must be the same');
+end
+
+% <boxes> is a struct array of mesh cells
+[newx0, newx1] = mitosis(b0, b1);
+for k = 1:2^dim,
+    boxes(k) = initbox(funname, ...
+                    newx0(k,:), newx1(k,:), pars, opt);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% THE MAIN LOOP
+% If ever boxes(i).LB > min([boxes.UB]), then box <i> can be discarded.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+COMPLETE = 0;   % completion flag
+plotlast = 0;   % keep track of when to plot
+displast = 0;   % keep track of when to verbose output
+histindx = 1;   % keep track of the history
+
+% save the history for analysis
+hist = repmat(struct('nfevals', NaN, 'nverts', NaN, 'ncells', ...
+    NaN, 'LB', NaN, 'UB', NaN, 'z', NaN, 'f', NaN, 'err', NaN), ...
+    opt.maxfeval, 1);
+
+% MAIN LOOP BEGIN
+while 1,
+
+    if COMPLETE, break; end
+
+    actives = find([boxes.status] == 1);
+    [~,bindx] = sort([boxes(actives).UB]);
+    bindx = actives(bindx);
+
+    for j = bindx,
+
+        % ITERATE on this cell (if it's still relevant)
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if boxes(j).status == 1,
+            for k = 1:opt.maxquadspercell-1,
+                boxes(j) = step(boxes(j));
+            end
+        end
+
+
+
+        % DECIDE whether to plot or display or not
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        nfevals = sum([boxes.nfevals]);
+        actives = find([boxes.status] == 1);
+        plotnow = nfevals - plotlast > opt.plotfreq;
+        dispnow = nfevals - displast > opt.dispfreq;
+        if plotnow, plotlast = nfevals; end
+        if dispnow, displast = nfevals; end
+
+        % PLOT all this beautiful stuff (if in 2 dimensions)
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if dim == 2 && plotnow,
+            for k = actives,
+                if k == actives(1),
+                    figure(1);
+                    clf; hold on;
+                    axis([b0(1) b1(1) b0(2) b1(2)]);
+                end
+                plot_graph(boxes(k));
+            end
+        end
+
+
+
+        % REMOVE any cells whose LBs exceed the global UB
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        actives = find([boxes.status] == 1);
+        indx = actives(find([boxes(actives).LB] > min([boxes.UB])));
+        for k = indx, % there is apparently no vectorized way of doing this
+            boxes(k).status = 0;
+        end
+
+        % SPLIT this box
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        boxes(j).status = -1;
+        [newx0, newx1] = mitosis(boxes(j).lb, boxes(j).ub);
+        for k = 1:2^dim,
+            boxes(length(boxes)+1) = initbox(funname, ...
+                                newx0(k,:), newx1(k,:), pars, opt);
+        end
+
+        % REMOVE any cells whose LBs exceed the global UB
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        actives = find([boxes.status] == 1);
+        indx = actives(find([boxes(actives).LB] > min([boxes.UB])));
+        for k = indx, % there is apparently no vectorized way of doing this
+            boxes(k).status = 0;
+        end
+
+        % SAVE some of the information
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        nfevals = sum([boxes.nfevals]);
+        actives = find([boxes.status] == 1);
+        aboxes = boxes(actives);
+        hist(histindx).nfevals = nfevals;
+        hist(histindx).nverts = sum([aboxes.heaplength]);
+        hist(histindx).ncells = length(actives);
+
+        indx = find([aboxes.UB] == min([aboxes.UB]), 1);
+        hist(histindx).LB = aboxes(indx).LB;
+        hist(histindx).UB = aboxes(indx).UB;
+        hist(histindx).z = aboxes(indx).quad(:,end);
+        hist(histindx).err = hist(histindx).UB - hist(histindx).LB;
+        hist(histindx).f = hist(histindx).UB; % might as well
+
+        % OUTPUT information at regular intervals
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if dispnow,
+            fprintf('nfevals:%d  acc:%.1e  UB:%.8f  #verts:%d  #cells:%d (%d)\n', ...
+                hist(histindx).nfevals, hist(histindx).err, ...
+                hist(histindx).UB, ...
+                hist(histindx).nverts, hist(histindx).ncells, ...
+                max([boxes(actives).iternum]));
+        end
+        
+        % actives = find([boxes.status] == 1);
+        % disp(['gUB:' num2str(min([boxes.UB])) ...
+        %     '  gLB:' num2str(max([boxes(actives).LB])) ]);
+
+
+        % CHECK the stopping criteria
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        if ( abs(hist(histindx).UB - hist(histindx).LB) <= opt.tol ...
+                    || hist(histindx).nfevals > opt.maxfeval ),
+            COMPLETE = 1;
+            break;
+        end
+
+        % if hist(histindx).nfevals > 500, keyboard, end
+
+        histindx = histindx + 1;
+
+    end
+
+end
+
+% chop off the NaNs
+indx = find(isnan([hist.UB]),1,'first');
+hist(indx:end) = [];
+
+% display final state
+hist(end)
+
+return
--- example.m
+++ example.m
@@ -11,10 +11,10 @@ opt.plotfreq = 20;          % plotting frequency (if 2D)
 opt.dispfreq = 1;           % verbose output frequency
 
 %%% MESH
-opt.splitting = 0;          % master flag for splitting
-opt.depthfirst = 0;         % flag for search type; 0 => breadth-first
+% opt.splitting = 1;          % master flag for splitting
+% opt.depthfirst = 0;         % flag for search type; 0 => breadth-first
 opt.mincellcount = 1;       % split all cells if there are this many or fewer
-opt.maxquadspercell = 20;   % maximum number of iterations on a single cell
+opt.maxquadspercell = 80;   % maximum number of iterations on a single cell
 
 %%% STOPPING CRITERIA
 opt.maxfeval = 2000;        % maximum iteration count, in a sense
@@ -22,6 +22,7 @@ opt.tol = 1e-6;             % desired level of accuracy
 
 %%% THE PROBLEM
 opt.gamma = -4;             % the gamma thing
+opt.minmax = 0;             % minimize (==0) or maximize (~=0) ?
 
 
 switch n
@@ -31,7 +32,7 @@ switch n
         pars.B = randn(225,100);
         lb = [-2 -pi];
         ub = [6 8-pi];
-        [r,bb] = eigopt_multi_mesh('fdist_uncont',lb,ub,pars,opt);
+        [r,bb] = eigopt_multi_mesh_depth('fdist_uncont',lb,ub,pars,opt);
         figure; plot_results(r);
         figure; hist([bb.iternum],15)
 
@@ -40,7 +41,7 @@ switch n
         pars.A = randn(10);
         lb = [-2 -pi];
         ub = [6 8-pi];
-        [r,bb] = eigopt_multi_mesh('fdist_defective',lb,ub,pars,opt);
+        [r,bb] = eigopt_multi_mesh_depth('fdist_defective',lb,ub,pars,opt);
         figure; plot_results(r);
         figure; hist([bb.iternum],15)
 
@@ -49,7 +50,7 @@ switch n
         pars.A = randn(10);
         lb = [-2 -pi];
         ub = [6 8-pi];
-        [r,bb] = eigopt_multi_mesh('fdist_triple',lb,ub,pars,opt);
+        [r,bb] = eigopt_multi_mesh_depth('fdist_triple',lb,ub,pars,opt);
         figure; plot_results(r);
         figure; hist([bb.iternum],15)
 
@@ -61,7 +62,7 @@ switch n
             pars.A(:,:,d) = A + A';
         end
         ee = ones(1,dim);
-        [r,bb] = eigopt_multi_mesh('minimize_max_multi',-2*ee,2*ee,pars,opt);
+        [r,bb] = eigopt_multi_mesh_depth('minimize_max_multi',-2*ee,2*ee,pars,opt);
         figure; plot_results(r);
         figure; hist([bb.iternum],15)
 end
